391e338b739641a938f333cebc621fa553d0dd84
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2018-01-01 01:16:31 +0100

    Bump version to 0.0.6

--- src/Saturn/AssemblyInfo.fs
+++ src/Saturn/AssemblyInfo.fs
@@ -5,13 +5,13 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Saturn")>]
 [<assembly: AssemblyProductAttribute("Saturn")>]
 [<assembly: AssemblyDescriptionAttribute("Opinionated, web development framework for F# which implements the server-side, functional MVC pattern")>]
-[<assembly: AssemblyVersionAttribute("0.0.5")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.5")>]
+[<assembly: AssemblyVersionAttribute("0.0.6")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.6")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Saturn"
     let [<Literal>] AssemblyProduct = "Saturn"
     let [<Literal>] AssemblyDescription = "Opinionated, web development framework for F# which implements the server-side, functional MVC pattern"
-    let [<Literal>] AssemblyVersion = "0.0.5"
-    let [<Literal>] AssemblyFileVersion = "0.0.5"
+    let [<Literal>] AssemblyVersion = "0.0.6"
+    let [<Literal>] AssemblyFileVersion = "0.0.6"
