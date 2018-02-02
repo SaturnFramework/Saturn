da1fe1d9069f982d5dd17eb8218935670ddd44d8
Krzysztof Cieslak <krzysztof_cieslak@windowslive.com>
2018-02-02 23:40:40 +0100

    Add sample for JWT

---
+++ sample/JWTSample/JWTSample.fs
@@ -0,0 +1,78 @@
+module JWTSample
+open System
+open Saturn.Application
+open Saturn.ControllerHelpers
+open System.Security.Claims
+open System.IdentityModel.Tokens.Jwt
+open Microsoft.IdentityModel.Tokens
+open Saturn.Router
+open Giraffe
+open Microsoft.AspNetCore.Http
+open Saturn.Pipeline
+
+//Based on https://medium.com/@dsincl12/json-web-token-with-giraffe-and-f-4cebe1c3ef3b
+
+let secret = "spadR2dre#u-ruBrE@TepA&*Uf@U"
+let issuer = "saturnframework.io"
+
+[<CLIMutable>]
+type LoginViewModel =
+    {
+        Email : string
+        Password : string
+    }
+
+[<CLIMutable>]
+type TokenResult =
+    {
+        Token : string
+    }
+
+let generateToken email = 
+    let claims = [|
+        Claim(JwtRegisteredClaimNames.Sub, email);
+        Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()) |] 
+    claims 
+    |> Authentication.generateToken (secret, SecurityAlgorithms.HmacSha256) issuer (DateTime.UtcNow.AddHours(1.0))
+
+let handleGetSecured =
+    fun (next : HttpFunc) (ctx : HttpContext) -> 
+        let email = ctx.User.FindFirst ClaimTypes.NameIdentifier
+        text ("User " + email.Value + " is authorized to access this resource.") next ctx
+
+let handlePostToken =
+    fun (next : HttpFunc) (ctx : HttpContext) ->
+        task {
+            let! model = ctx.BindJsonAsync<LoginViewModel>()
+
+            // authenticate user
+            
+            let tokenResult = generateToken model.Email
+
+            return! json tokenResult next ctx
+}
+
+let securedRouter = scope {
+    pipe_through jwtAuthentication
+    get "/" handleGetSecured 
+}
+
+let topRouter = scope {
+    error_handler (setStatusCode 404 >=> text "Not Found")
+    
+    post "/token" handlePostToken
+    get "/" (text "public route")
+    forward "/secured" securedRouter
+}
+
+let app = application {
+    use_jwt_authentication secret issuer  
+
+    router topRouter
+    url "http://0.0.0.0:8085/"
+}
+
+[<EntryPoint>]
+let main _ =
+    run app
+    0
