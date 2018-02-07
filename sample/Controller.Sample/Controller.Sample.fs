d4476114eb1658b4f1dbc537fafe7a3a43d4facf
Krzysztof Cieslak <krzysztof_cieslak@windowslive.com>
2018-02-07 15:15:32 +0100

    Use .Net Core for sample apps

--- sample/Controller.Sample/Controller.Sample.fs
+++ sample/Controller.Sample/Controller.Sample.fs
@@ -7,29 +7,29 @@ open Giraffe.HttpHandlers
 open Saturn.Application
 
 let commentController userId = controller {
-    index (fun ctx -> (sprintf "Comment Index handler for user %s" userId ) |> Controller.text ctx)
-    add (fun ctx -> (sprintf "Comment Add handler for user %s" userId ) |> Controller.text ctx)
-    show (fun (ctx, id) -> (sprintf "Show comment %s handler for user %s" id userId ) |> Controller.text ctx)
-    edit (fun (ctx, id) -> (sprintf "Edit comment %s handler for user %s" id userId )  |> Controller.text ctx)
+    index (fun ctx -> (sprintf "Comment Index handler for user %i" userId ) |> Controller.text ctx)
+    add (fun ctx -> (sprintf "Comment Add handler for user %i" userId ) |> Controller.text ctx)
+    show (fun (ctx, id) -> (sprintf "Show comment %s handler for user %i" id userId ) |> Controller.text ctx)
+    edit (fun (ctx, id) -> (sprintf "Edit comment %s handler for user %i" id userId )  |> Controller.text ctx)
 }
  
 let userControllerVersion1 = controller {
     version 1
-    subController "comments" commentController 
+    subController "/comments" commentController 
 
     index (fun ctx -> "Index handler version 1" |> Controller.text ctx)
     add (fun ctx -> "Add handler version 1" |> Controller.text ctx)
-    show (fun (ctx, id) -> (sprintf "Show handler version 1 - %s" id) |> Controller.text ctx)
-    edit (fun (ctx, id) -> (sprintf "Edit handler version 1 - %s" id) |> Controller.text ctx)
+    show (fun (ctx, id) -> (sprintf "Show handler version 1 - %i" id) |> Controller.text ctx)
+    edit (fun (ctx, id) -> (sprintf "Edit handler version 1 - %i" id) |> Controller.text ctx)
 }
 
 let userController = controller {   
-    subController "comments" commentController     
+    subController "/comments" commentController     
 
     index (fun ctx -> "Index handler no version" |> Controller.text ctx)
     add (fun ctx -> "Add handler no version" |> Controller.text ctx)
-    show (fun (ctx, id) -> (sprintf "Show handler no version - %s" id) |> Controller.text ctx)
-    edit (fun (ctx, id) -> (sprintf "Edit handler no version - %s" id) |> Controller.text ctx)
+    show (fun (ctx, id) -> (sprintf "Show handler no version - %i" id) |> Controller.text ctx)
+    edit (fun (ctx, id) -> (sprintf "Edit handler no version - %i" id) |> Controller.text ctx)
 }
 
 let topRouter = scope {
@@ -46,5 +46,5 @@ let app = application {
 
 [<EntryPoint>]
 let main _ =
-    run app
+    run app 
     0
