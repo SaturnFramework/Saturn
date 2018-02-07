ac026a60387ea538c792fc9147d5acb124d3df46
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2018-02-08 00:06:53 +0100

    Add plugs per action to controller

--- src/Saturn/Controller.fs
+++ src/Saturn/Controller.fs
@@ -5,6 +5,16 @@ module Controller =
   open Microsoft.AspNetCore.Http
   open Giraffe
 
+  type Action =
+    | Index
+    | Show
+    | Add
+    | Edit
+    | Create
+    | Update
+    | Delete
+    | All
+
   type ControllerState<'Key> = {
     Index: (HttpContext -> HttpFuncResult) option
     Show: (HttpContext * 'Key -> HttpFuncResult) option
@@ -15,6 +25,7 @@ module Controller =
     Delete: (HttpContext * 'Key -> HttpFuncResult) option
     NotFoundHandler: HttpHandler option
     SubControllers : (string * ('Key -> HttpHandler)) list
+    Plugs : Map<Action, HttpHandler list>
     Version: int option
   }
 
@@ -29,7 +40,7 @@ module Controller =
 
   type ControllerBuilder<'Key> internal () =
     member __.Yield(_) : ControllerState<'Key> =
-      { Index = None; Show = None; Add = None; Edit = None; Create = None; Update = None; Delete = None; NotFoundHandler = None; Version = None; SubControllers = [] }
+      { Index = None; Show = None; Add = None; Edit = None; Create = None; Update = None; Delete = None; NotFoundHandler = None; Version = None; SubControllers = []; Plugs = Map.empty<_,_>  }
 
     member __.Run(state : ControllerState<'Key>) : HttpHandler =
       let typ =
@@ -43,99 +54,104 @@ module Controller =
         else
           failwithf "Couldn't create router for controller. Key type not supported."
 
+      let addPlugs action handler =
+        match state.Plugs.TryFind action with
+        | Some acts -> (succeed |> List.foldBack (>=>) acts ) >=> handler
+        | None -> handler
+
       let lst =
         choose [
           for (sPath, sCs) in state.SubControllers do
             match typ with
             | Bool ->
-              yield routef (PrintfFormat<bool -> obj, obj, obj, obj, bool>("/%b" + sPath)) (fun input -> subRoute ("/" + (string input) + sPath) (sCs (unbox<'Key> input)) )
-              yield routef (PrintfFormat<bool -> string -> obj, obj, obj, obj, bool * string>("/%b" + sPath + "%s")) (fun (input, _) -> subRoute ("/" + (string input) + sPath) (sCs (unbox<'Key> input)) )
+              yield routef (PrintfFormat<bool -> obj, obj, obj, obj, bool>("/%b" + sPath)) (fun input -> subRoute ("/" + (string input) + sPath) (sCs (unbox<'Key> input)))
+              yield routef (PrintfFormat<bool -> string -> obj, obj, obj, obj, bool * string>("/%b" + sPath + "%s")) (fun (input, _) -> subRoute ("/" + (string input) + sPath) (sCs (unbox<'Key> input)))
             | Char ->
-              yield routef (PrintfFormat<char -> obj, obj, obj, obj, char>("/%c" + sPath)) (fun input -> subRoute ("/" + (string input) + sPath) (sCs (unbox<'Key> input)) )
-              yield routef (PrintfFormat<char -> string -> obj, obj, obj, obj, char * string>("/%c" + sPath + "%s")) (fun (input, _) -> subRoute ("/" + (string input) + sPath) (sCs (unbox<'Key> input)) )
+              yield routef (PrintfFormat<char -> obj, obj, obj, obj, char>("/%c" + sPath)) (fun input -> subRoute ("/" + (string input) + sPath) (sCs (unbox<'Key> input)))
+              yield routef (PrintfFormat<char -> string -> obj, obj, obj, obj, char * string>("/%c" + sPath + "%s")) (fun (input, _) -> subRoute ("/" + (string input) + sPath) (sCs (unbox<'Key> input)))
             | String ->
-              yield routef (PrintfFormat<string -> obj, obj, obj, obj, string>("/%s" + sPath)) (fun input -> subRoute ("/" + (string input) + sPath) (sCs (unbox<'Key> input)) )
-              yield routef (PrintfFormat<string -> string -> obj, obj, obj, obj, string * string>("/%s" + sPath + "%s")) (fun (input, _) -> subRoute ("/" + (string input) + sPath) (sCs (unbox<'Key> input)) )
+              yield routef (PrintfFormat<string -> obj, obj, obj, obj, string>("/%s" + sPath)) (fun input -> subRoute ("/" + (string input) + sPath) (sCs (unbox<'Key> input)))
+              yield routef (PrintfFormat<string -> string -> obj, obj, obj, obj, string * string>("/%s" + sPath + "%s")) (fun (input, _) -> subRoute ("/" + (string input) + sPath) (sCs (unbox<'Key> input)))
             | Int32 ->
-              yield routef (PrintfFormat<int -> obj, obj, obj, obj, int>("/%i" + sPath)) (fun input -> subRoute ("/" + (string input) + sPath) (sCs (unbox<'Key> input)) )
-              yield routef (PrintfFormat<int -> string -> obj, obj, obj, obj, int * string>("/%i" + sPath + "%s")) (fun (input, _) -> subRoute ("/" + (string input) + sPath) (sCs (unbox<'Key> input)) )
+              yield routef (PrintfFormat<int -> obj, obj, obj, obj, int>("/%i" + sPath)) (fun input -> subRoute ("/" + (string input) + sPath) (sCs (unbox<'Key> input)))
+              yield routef (PrintfFormat<int -> string -> obj, obj, obj, obj, int * string>("/%i" + sPath + "%s")) (fun (input, _) -> subRoute ("/" + (string input) + sPath) (sCs (unbox<'Key> input)))
             | Int64 ->
-              yield routef (PrintfFormat<int64 -> obj, obj, obj, obj, int64>("/%d" + sPath)) (fun input -> subRoute ("/" + (string input) + sPath) (sCs (unbox<'Key> input)) )
-              yield routef (PrintfFormat<int64 -> string -> obj, obj, obj, obj, int64 * string>("/%d" + sPath + "%s")) (fun (input, _) -> subRoute ("/" + (string input) + sPath) (sCs (unbox<'Key> input)) )
+              yield routef (PrintfFormat<int64 -> obj, obj, obj, obj, int64>("/%d" + sPath)) (fun input -> subRoute ("/" + (string input) + sPath) (sCs (unbox<'Key> input)))
+              yield routef (PrintfFormat<int64 -> string -> obj, obj, obj, obj, int64 * string>("/%d" + sPath + "%s")) (fun (input, _) -> subRoute ("/" + (string input) + sPath) (sCs (unbox<'Key> input)))
             | Float ->
-              yield routef (PrintfFormat<float -> obj, obj, obj, obj, float>("/%f" + sPath)) (fun input -> subRoute ("/" + (string input) + sPath) (sCs (unbox<'Key> input)) )
-              yield routef (PrintfFormat<float -> string -> obj, obj, obj, obj, float * string>("/%f" + sPath + "%s")) (fun (input, _) -> subRoute ("/" + (string input) + sPath) (sCs (unbox<'Key> input)) )
+              yield routef (PrintfFormat<float -> obj, obj, obj, obj, float>("/%f" + sPath)) (fun input -> subRoute ("/" + (string input) + sPath) (sCs (unbox<'Key> input)))
+              yield routef (PrintfFormat<float -> string -> obj, obj, obj, obj, float * string>("/%f" + sPath + "%s")) (fun (input, _) -> subRoute ("/" + (string input) + sPath) (sCs (unbox<'Key> input)))
             | Guid ->
-              yield routef (PrintfFormat<obj -> obj, obj, obj, obj, obj>("/%O" + sPath)) (fun input -> subRoute ("/" + (string input) + sPath) (sCs (unbox<'Key> input)) )
-              yield routef (PrintfFormat<obj -> string -> obj, obj, obj, obj, obj * string>("/%O" + sPath + "%s")) (fun (input, _) -> subRoute ("/" + (string input) + sPath) (sCs (unbox<'Key> input)) )
+              yield routef (PrintfFormat<obj -> obj, obj, obj, obj, obj>("/%O" + sPath)) (fun input -> subRoute ("/" + (string input) + sPath) (sCs (unbox<'Key> input)))
+              yield routef (PrintfFormat<obj -> string -> obj, obj, obj, obj, obj * string>("/%O" + sPath + "%s")) (fun (input, _) -> subRoute ("/" + (string input) + sPath) (sCs (unbox<'Key> input)))
           yield GET >=> choose [
-            if state.Add.IsSome then yield route "/add" >=> (fun _ ctx -> state.Add.Value(ctx))
+            if state.Add.IsSome then yield addPlugs Add (route "/add" >=> (fun _ ctx -> state.Add.Value(ctx)))
             if state.Edit.IsSome then
               match typ with
-              | Bool -> yield routef "/%b/edit" (fun input _ ctx -> state.Edit.Value(ctx, unbox<'Key> input) )
-              | Char -> yield routef "/%c/edit" (fun input _ ctx -> state.Edit.Value(ctx, unbox<'Key> input) )
-              | String -> yield routef "/%s/edit" (fun input _ ctx -> state.Edit.Value(ctx, unbox<'Key> input) )
-              | Int32 -> yield routef "/%i/edit" (fun input _ ctx -> state.Edit.Value(ctx, unbox<'Key> input) )
-              | Int64 -> yield routef "/%d/edit" (fun input _ ctx -> state.Edit.Value(ctx, unbox<'Key> input) )
-              | Float -> yield routef "/%f/edit" (fun input _ ctx -> state.Edit.Value(ctx, unbox<'Key> input) )
-              | Guid -> yield routef "/%O/edit" (fun input _ ctx -> state.Edit.Value(ctx, unbox<'Key> input) )
+              | Bool -> yield addPlugs Edit (routef "/%b/edit" (fun input _ ctx -> state.Edit.Value(ctx, unbox<'Key> input)))
+              | Char -> yield addPlugs Edit (routef "/%c/edit" (fun input _ ctx -> state.Edit.Value(ctx, unbox<'Key> input)))
+              | String -> yield addPlugs Edit (routef "/%s/edit" (fun input _ ctx -> state.Edit.Value(ctx, unbox<'Key> input)))
+              | Int32 -> yield addPlugs Edit (routef "/%i/edit" (fun input _ ctx -> state.Edit.Value(ctx, unbox<'Key> input)))
+              | Int64 -> yield addPlugs Edit (routef "/%d/edit" (fun input _ ctx -> state.Edit.Value(ctx, unbox<'Key> input)))
+              | Float -> yield addPlugs Edit (routef "/%f/edit" (fun input _ ctx -> state.Edit.Value(ctx, unbox<'Key> input)))
+              | Guid -> yield addPlugs Edit (routef "/%O/edit" (fun input _ ctx -> state.Edit.Value(ctx, unbox<'Key> input)))
             if state.Show.IsSome then
               match typ with
-              | Bool -> yield routef "/%b" (fun input _ ctx -> state.Show.Value(ctx, unbox<'Key> input) )
-              | Char -> yield routef "/%c" (fun input _ ctx -> state.Show.Value(ctx, unbox<'Key> input) )
-              | String -> yield routef "/%s" (fun input _ ctx -> state.Show.Value(ctx, unbox<'Key> input) )
-              | Int32 -> yield routef "/%i" (fun input _ ctx -> state.Show.Value(ctx, unbox<'Key> input) )
-              | Int64 -> yield routef "/%d" (fun input _ ctx -> state.Show.Value(ctx, unbox<'Key> input) )
-              | Float -> yield routef "/%f" (fun input _ ctx -> state.Show.Value(ctx, unbox<'Key> input) )
-              | Guid -> yield routef "/%O" (fun input _ ctx -> state.Show.Value(ctx, unbox<'Key> input) )
+              | Bool -> yield addPlugs Show (routef "/%b" (fun input _ ctx -> state.Show.Value(ctx, unbox<'Key> input)))
+              | Char -> yield addPlugs Show (routef "/%c" (fun input _ ctx -> state.Show.Value(ctx, unbox<'Key> input)))
+              | String -> yield addPlugs Show (routef "/%s" (fun input _ ctx -> state.Show.Value(ctx, unbox<'Key> input)))
+              | Int32 -> yield addPlugs Show (routef "/%i" (fun input _ ctx -> state.Show.Value(ctx, unbox<'Key> input)))
+              | Int64 -> yield addPlugs Show (routef "/%d" (fun input _ ctx -> state.Show.Value(ctx, unbox<'Key> input)))
+              | Float -> yield addPlugs Show (routef "/%f" (fun input _ ctx -> state.Show.Value(ctx, unbox<'Key> input)))
+              | Guid -> yield addPlugs Show (routef "/%O" (fun input _ ctx -> state.Show.Value(ctx, unbox<'Key> input)))
             if state.Index.IsSome then
-              yield route "" >=> (fun _ ctx -> ctx.Request.Path <- PathString(ctx.Request.Path.ToString() + "/"); state.Index.Value(ctx))
-              yield route "/" >=> (fun _ ctx -> state.Index.Value(ctx))
+              yield addPlugs Index (route "" >=> (fun _ ctx -> ctx.Request.Path <- PathString(ctx.Request.Path.ToString() + "/"); state.Index.Value(ctx)))
+              yield addPlugs Index (route "/" >=> (fun _ ctx -> state.Index.Value(ctx)))
           ]
           yield POST >=> choose [
-            if state.Create.IsSome then yield route "/" >=> (fun _ ctx -> state.Create.Value(ctx))
+            if state.Create.IsSome then yield addPlugs Create (route "/" >=> (fun _ ctx -> state.Create.Value(ctx)))
             if state.Update.IsSome then
               match typ with
-              | Bool -> yield routef "/%b" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input) )
-              | Char -> yield routef "/%c" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input) )
-              | String -> yield routef "/%s" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input) )
-              | Int32 -> yield routef "/%i" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input) )
-              | Int64 -> yield routef "/%d" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input) )
-              | Float -> yield routef "/%f" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input) )
-              | Guid -> yield routef "/%O" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input) )
+              | Bool -> yield addPlugs Update (routef "/%b" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input)))
+              | Char -> yield addPlugs Update (routef "/%c" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input)))
+              | String -> yield addPlugs Update (routef "/%s" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input)))
+              | Int32 -> yield addPlugs Update (routef "/%i" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input)))
+              | Int64 -> yield addPlugs Update (routef "/%d" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input)))
+              | Float -> yield addPlugs Update (routef "/%f" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input)))
+              | Guid -> yield addPlugs Update (routef "/%O" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input)))
           ]
           yield PATCH >=> choose [
             if state.Update.IsSome then
               match typ with
-              | Bool -> yield routef "/%b" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input) )
-              | Char -> yield routef "/%c" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input) )
-              | String -> yield routef "/%s" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input) )
-              | Int32 -> yield routef "/%i" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input) )
-              | Int64 -> yield routef "/%d" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input) )
-              | Float -> yield routef "/%f" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input) )
-              | Guid -> yield routef "/%O" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input) )
+              | Bool -> yield addPlugs Update (routef "/%b" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input)))
+              | Char -> yield addPlugs Update (routef "/%c" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input)))
+              | String -> yield addPlugs Update (routef "/%s" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input)))
+              | Int32 -> yield addPlugs Update (routef "/%i" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input)))
+              | Int64 -> yield addPlugs Update (routef "/%d" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input)))
+              | Float -> yield addPlugs Update (routef "/%f" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input)))
+              | Guid -> yield addPlugs Update (routef "/%O" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input)))
           ]
           yield PUT >=> choose [
             if state.Update.IsSome then
               match typ with
-              | Bool -> yield routef "/%b" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input) )
-              | Char -> yield routef "/%c" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input) )
-              | String -> yield routef "/%s" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input) )
-              | Int32 -> yield routef "/%i" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input) )
-              | Int64 -> yield routef "/%d" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input) )
-              | Float -> yield routef "/%f" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input) )
-              | Guid -> yield routef "/%O" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input) )
+              | Bool -> yield addPlugs Update (routef "/%b" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input)))
+              | Char -> yield addPlugs Update (routef "/%c" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input)))
+              | String -> yield addPlugs Update (routef "/%s" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input)))
+              | Int32 -> yield addPlugs Update (routef "/%i" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input)))
+              | Int64 -> yield addPlugs Update (routef "/%d" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input)))
+              | Float -> yield addPlugs Update (routef "/%f" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input)))
+              | Guid -> yield addPlugs Update (routef "/%O" (fun input _ ctx -> state.Update.Value(ctx, unbox<'Key> input)))
           ]
           yield DELETE >=> choose [
             if state.Delete.IsSome then
               match typ with
-              | Bool -> yield routef "/%b" (fun input _ ctx -> state.Delete.Value(ctx, unbox<'Key> input) )
-              | Char -> yield routef "/%c" (fun input _ ctx -> state.Delete.Value(ctx, unbox<'Key> input) )
-              | String -> yield routef "/%s" (fun input _ ctx -> state.Delete.Value(ctx, unbox<'Key> input) )
-              | Int32 -> yield routef "/%i" (fun input _ ctx -> state.Delete.Value(ctx, unbox<'Key> input) )
-              | Int64 -> yield routef "/%d" (fun input _ ctx -> state.Delete.Value(ctx, unbox<'Key> input) )
-              | Float -> yield routef "/%f" (fun input _ ctx -> state.Delete.Value(ctx, unbox<'Key> input) )
-              | Guid -> yield routef "/%O" (fun input _ ctx -> state.Delete.Value(ctx, unbox<'Key> input) )
+              | Bool -> yield addPlugs Delete (routef "/%b" (fun input _ ctx -> state.Delete.Value(ctx, unbox<'Key> input)))
+              | Char -> yield addPlugs Delete (routef "/%c" (fun input _ ctx -> state.Delete.Value(ctx, unbox<'Key> input)))
+              | String -> yield addPlugs Delete (routef "/%s" (fun input _ ctx -> state.Delete.Value(ctx, unbox<'Key> input)))
+              | Int32 -> yield addPlugs Delete (routef "/%i" (fun input _ ctx -> state.Delete.Value(ctx, unbox<'Key> input)))
+              | Int64 -> yield addPlugs Delete (routef "/%d" (fun input _ ctx -> state.Delete.Value(ctx, unbox<'Key> input)))
+              | Float -> yield addPlugs Delete (routef "/%f" (fun input _ ctx -> state.Delete.Value(ctx, unbox<'Key> input)))
+              | Guid -> yield addPlugs Delete (routef "/%O" (fun input _ ctx -> state.Delete.Value(ctx, unbox<'Key> input)))
           ]
           if state.NotFoundHandler.IsSome then yield state.NotFoundHandler.Value
       ]
@@ -192,6 +208,23 @@ module Controller =
     [<CustomOperation("subController")>]
     member __.SubController(state, path, handler) =
       {state with SubControllers = (path, handler)::state.SubControllers}
+
+    [<CustomOperation("plug")>]
+    member __.Plug(state, actions, handler) =
+      let addPlug state action handler =
+        let newplugs =
+          if state.Plugs.ContainsKey action then
+            state.Plugs.Add(action, (handler::state.Plugs.[action]))
+          else
+            state.Plugs.Add(action,[handler])
+        {state with Plugs = newplugs}
+
+
+      if actions |> List.contains All then
+        [Index; Show; Add; Edit; Create; Update; Delete] |> List.fold (fun acc e -> addPlug acc e handler) state
+      else
+        actions |> List.fold (fun acc e -> addPlug acc e handler) state
+
 
   let controller<'Key> = ControllerBuilder<'Key> ()
 
