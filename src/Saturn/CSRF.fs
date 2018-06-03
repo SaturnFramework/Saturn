f2553e62f6caa98f1b350b2a8b5da5f51334fc89
Chet Husk <chusk3@gmail.com>
2018-06-03 13:00:19 -0500

    finish view helpers and giraffe view engine sample

--- src/Saturn/CSRF.fs
+++ src/Saturn/CSRF.fs
@@ -1,12 +1,13 @@
 namespace Saturn
 
-open FSharp.Control.Tasks.ContextInsensitive
-open Giraffe.Core
-open Giraffe.ResponseWriters
-open Microsoft.AspNetCore.Antiforgery
-open Microsoft.AspNetCore.Http
-
 module CSRF =
+  open FSharp.Control.Tasks.ContextInsensitive
+  open Giraffe.Core
+  open Giraffe.ResponseWriters
+  open Microsoft.AspNetCore.Antiforgery
+  open Microsoft.AspNetCore.Http
+  open Microsoft.Extensions.Logging
+
   let private shouldValidate  =
     let unprotectedMethods = Set.ofList ["GET"; "HEAD"; "TRACE"; "OPTIONS"]
     fun (ctx: HttpContext) -> not <| unprotectedMethods.Contains ctx.Request.Method
@@ -15,6 +16,18 @@ module CSRF =
     setStatusCode 403
     >=> text ex.Message
 
+  let inline private logMissingAntiforgeryFeature (ctx: HttpContext) =
+    let logger = ctx.GetService<ILoggerFactory>().CreateLogger("Saturn.CSRF")
+    logger.LogWarning(
+      """The `IAntiforgery` feature could not be resolved. Are you sure you have added it to the current application? For example:
+  application {
+    use_antiforgery
+  },
+or
+  application {
+    use_antiforgery_with_config (fun options -> options.HeaderName <- "X-XSRF-TOKEN")
+  }
+      """)
   /// Protect a resource by validating that requests that can change state come with a valid request antiforgery token, which is based off of a known session token.
   /// The particular configuration options can be set via the `application` builder's `use_antiforgery_with_config` method.
   let csrf : HttpHandler =
@@ -22,8 +35,13 @@ module CSRF =
      if shouldValidate ctx
      then
       try
-        do! ctx.GetService<IAntiforgery>().ValidateRequestAsync(ctx)
-        return! next ctx
+        match ctx.GetService<IAntiforgery>() with
+        | null ->
+          logMissingAntiforgeryFeature ctx
+          return! setStatusCode 500 next ctx
+        | antiforgery ->
+          do! antiforgery.ValidateRequestAsync(ctx)
+          return! next ctx
       with
       | :? AntiforgeryValidationException as ex ->
         return! sendException ex next ctx
@@ -33,4 +51,24 @@ module CSRF =
 
   let getRequestTokens (ctx: HttpContext) = ctx.GetService<IAntiforgery>().GetAndStoreTokens(ctx)
 
+  /// Contains view helpers for csrf tokens for various view engines.
+  module View =
+    module Giraffe =
+      open Giraffe.GiraffeViewEngine
 
+      ///Creates a csrf token form input of the kind: <input type="hidden" name="TOKEN_NAME" value="TOKEN_VALUE" />
+      let csrfTokenInput (ctx: HttpContext) =
+        match ctx.GetService<IAntiforgery>() with
+        | null ->
+          logMissingAntiforgeryFeature ctx
+          input [ _name "Missing Antiforgery Feature"
+                  _value "No Antiforgery Token Available, check your application configuration"
+                  _type "text" ]
+        | antiforgery ->
+          let tokens = antiforgery.GetAndStoreTokens(ctx)
+          input [ _name tokens.FormFieldName
+                  _value tokens.RequestToken
+                  _type "hidden" ]
+
+      ///View helper for creating a form that implicitly inserts a CSRF token hidden form input.
+      let protectedForm ctx attrs children = form attrs (csrfTokenInput ctx :: children)
