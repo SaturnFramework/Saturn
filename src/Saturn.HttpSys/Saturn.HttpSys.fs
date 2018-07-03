3eb9f9133afeb1039fc8e60f8cb6b6a0d972d53d
Christian Steinert <chr.steinert@outlook.com>
2018-07-03 19:23:06 +0200

    Adds HttpSys project and sample.

---
+++ src/Saturn.HttpSys/Saturn.HttpSys.fs
@@ -0,0 +1,39 @@
+module Saturn
+
+open Saturn
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.AspNetCore.Server.HttpSys
+
+type ApplicationBuilder with
+
+    [<CustomOperation("use_httpsys")>]
+    member __.UseHttpSys(state) =
+        let host (builder: IWebHostBuilder) =
+            builder.UseHttpSys ()
+        { state with
+            HostConfigs = host::state.HostConfigs
+        }
+
+    [<CustomOperation("use_httpsys_with_args")>]
+    member __.UseHttpSysWithArgs(state, args) =
+        let host (builder: IWebHostBuilder) =
+            builder.UseHttpSys args
+        { state with
+            HostConfigs = host::state.HostConfigs
+        }
+
+    [<CustomOperation("use_httpsys_windows_auth")>]
+    member __.UserHttpSysWindowsAuth(state, allowAnonymous: bool) =
+        let host (builder: IWebHostBuilder) =
+            builder.UseHttpSys (fun c ->
+                c.Authentication.AllowAnonymous <- allowAnonymous
+                c.Authentication.Schemes <- AuthenticationSchemes.Negotiate ||| AuthenticationSchemes.NTLM)
+        let service (builder: IServiceCollection) =
+            builder.AddAuthentication HttpSysDefaults.AuthenticationScheme |> ignore
+            builder
+        { state with
+            HostConfigs = host::state.HostConfigs
+            ServicesConfig = service::state.ServicesConfig
+        }
+
