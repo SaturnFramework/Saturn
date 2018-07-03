808c58a24a93a557192e6cb448f2d5509afd4320
Christian Steinert <chr.steinert@outlook.com>
2018-07-03 19:23:06 +0200

    Adds HttpSys project and sample.

---
+++ sample/WindowsAuthSample/WindowsAuthSample.fs
@@ -0,0 +1,20 @@
+module WindowsAuthSample
+
+open Saturn
+open Giraffe.ResponseWriters
+
+let app = application {
+    router (fun f c -> 
+                let message = sprintf "hello %s!" c.User.Identity.Name
+                text message f c)
+    url "http://localhost:8085/"
+    memory_cache
+    use_gzip
+    disable_diagnostics
+    use_httpsys_windows_auth false
+}
+
+[<EntryPoint>]
+let main _ =
+    run app
+    0 // return an integer exit code
