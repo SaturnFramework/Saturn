ab672f8a188e30639cc1c1294fe99e190a0a77eb
Robin Munn <rmunn@pobox.com>
2018-07-05 05:50:13 +0700

    Add Google OAuth

--- src/Saturn/Application.fs
+++ src/Saturn/Application.fs
@@ -17,6 +17,7 @@ open Microsoft.AspNetCore.Authentication.Cookies
 open Microsoft.AspNetCore.Http
 open Microsoft.AspNetCore.Authentication
 open Microsoft.AspNetCore.Authorization
+open Microsoft.AspNetCore.Authentication.Google
 open System.Net.Http
 open System.Net.Http.Headers
 open Newtonsoft.Json.Linq
@@ -263,7 +264,7 @@ module Application =
       }
 
     ///Enables default GitHub OAuth authentication.
-    ///`jsonToClaimMap` should contain sequance of tupples where first element is a name of the of the key in JSON object and second element is a name of the claim.
+    ///`jsonToClaimMap` should contain sequance of tuples where first element is a name of the of the key in JSON object and second element is a name of the claim.
     ///For example: `["login", "githubUsername"; "name", "fullName"]` where `login` and `name` are names of fields in GitHub JSON response (https://developer.github.com/v3/users/#get-the-authenticated-user).
     [<CustomOperation("use_github_oauth")>]
     member __.UseGithubAuth(state: ApplicationState, clientId : string, clientSecret : string, callbackPath : string, jsonToClaimMap : (string * string) seq) =
@@ -321,6 +322,49 @@ module Application =
           cfg.DefaultChallengeScheme <- "GitHub")
          .AddCookie()
          .AddOAuth("GitHub",config) |> ignore
+        s
+
+      { state with
+          ServicesConfig = service::state.ServicesConfig
+          AppConfigs = middleware::state.AppConfigs
+      }
+
+    ///Enables default Google OAuth authentication.
+    ///`jsonToClaimMap` should contain sequance of tuples where first element is a name of the of the key in JSON object and second element is a name of the claim.
+    ///For example: `["id", ClaimTypes.NameIdentifier; "displayName", ClaimTypes.Name]` where `id` and `displayName` are names of fields in the Google JSON response (https://developers.google.com/+/web/api/rest/latest/people#resource).
+    [<CustomOperation("use_google_oauth")>]
+    member __.UseGoogleAuth(state: ApplicationState, clientId : string, clientSecret : string, callbackPath : string, jsonToClaimMap : (string * string) seq) =
+      let middleware (app : IApplicationBuilder) =
+        app.UseAuthentication()
+
+      let service (s : IServiceCollection) =
+        s.AddAuthentication(fun cfg ->
+          cfg.DefaultScheme <- CookieAuthenticationDefaults.AuthenticationScheme
+          cfg.DefaultSignInScheme <- CookieAuthenticationDefaults.AuthenticationScheme
+          cfg.DefaultChallengeScheme <- "Google")
+         .AddCookie()
+         .AddGoogle(fun opt ->
+          opt.ClientId <- clientId
+          opt.ClientSecret <- clientSecret
+          opt.CallbackPath <- PathString(callbackPath)
+          jsonToClaimMap |> Seq.iter (fun (k,v) -> opt.ClaimActions.MapJsonKey(v,k) )
+          let ev = opt.Events
+
+          ev.OnCreatingTicket <-
+            fun ctx ->
+              let tsk = task {
+                let req = new HttpRequestMessage(HttpMethod.Get, ctx.Options.UserInformationEndpoint)
+                req.Headers.Accept.Add(MediaTypeWithQualityHeaderValue("application/json"))
+                req.Headers.Authorization <- AuthenticationHeaderValue("Bearer", ctx.AccessToken)
+                let! (response : HttpResponseMessage) = ctx.Backchannel.SendAsync(req, HttpCompletionOption.ResponseHeadersRead, ctx.HttpContext.RequestAborted)
+                response.EnsureSuccessStatusCode () |> ignore
+                let! cnt = response.Content.ReadAsStringAsync()
+                let user = JObject.Parse cnt
+                ctx.RunClaimActions user
+              }
+              Task.Factory.StartNew(fun () -> tsk.Result)
+
+         ) |> ignore
         s
 
       { state with
