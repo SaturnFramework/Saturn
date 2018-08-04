05fd7a814b1008ff33c1d10501ca6de318ad7ea9
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2018-08-04 14:32:02 +0200

    Add Turbolinks sample

---
+++ sample/TurbolinksSample/TurbolinksSample.fs
@@ -0,0 +1,37 @@
+module WindowsAuthSample
+
+open Saturn
+open Giraffe.ResponseWriters
+open Giraffe.Core
+
+let browser = pipeline {
+    plug acceptHtml
+    plug putSecureBrowserHeaders
+    set_header "x-pipeline-type" "Browser"
+}
+
+let defaultView = router {
+    get "/" (htmlView Index.layout)
+    get "/index.html" (redirectTo false "/")
+    get "/default.html" (redirectTo false "/")
+}
+
+let browserRouter = router {
+    pipe_through browser
+
+    forward "" defaultView
+    forward "/otherView" (htmlView OtherView.layout)
+}
+
+
+let app = application {
+    use_router browserRouter
+    url "http://localhost:8085/"
+    use_static "static"
+    use_turbolinks
+}
+
+[<EntryPoint>]
+let main _ =
+    run app
+    0 // return an integer exit code
