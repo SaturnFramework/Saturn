3d4a25fc9f91606c0fbd31fe04fb50177736a656
Nino Floris <mail@ninofloris.com>
2018-08-19 22:14:32 +0200

    Fix indenting to match project wide 2 spaces convention

--- src/Saturn.Extensions.Turbolinks/Turbolinks.fs
+++ src/Saturn.Extensions.Turbolinks/Turbolinks.fs
@@ -8,56 +8,54 @@ open Giraffe
 open Microsoft.Extensions.Primitives
 
 module TurbolinksHelpers =
-    let isXhr (ctx: HttpContext) =
-        ctx.Request.Headers.["X-Requested-With"].ToString() = "XMLHttpRequest"
+  let isXhr (ctx: HttpContext) =
+    ctx.Request.Headers.["X-Requested-With"].ToString() = "XMLHttpRequest"
 
-    let isTurbolink (ctx: HttpContext) =
-        ctx.Request.Headers.ContainsKey "Turbolinks-Referrer"
+  let isTurbolink (ctx: HttpContext) =
+    ctx.Request.Headers.ContainsKey "Turbolinks-Referrer"
 
-    let js (ctx: HttpContext, data) =
-        ctx.SetContentType "text/javascript"
-        ctx.WriteStringAsync data
+  let js (ctx: HttpContext, data) =
+    ctx.SetContentType "text/javascript"
+    ctx.WriteStringAsync data
 
-    let internal turbolinksResp (t, m) =
-        if m = "GET" then
-            sprintf "Turbolinks.visit('%s');" t
-        else
-            sprintf "Turbolinks.clearCache();\nTurbolinks.visit('%s');" t
+  let internal turbolinksResp (t, m) =
+    if m = "GET" then
+      sprintf "Turbolinks.visit('%s');" t
+    else
+      sprintf "Turbolinks.clearCache();\nTurbolinks.visit('%s');" t
 
-    let redirect ctx path : HttpFuncResult=
-        if isXhr ctx then
-            ctx.SetStatusCode 200
-            js(ctx, turbolinksResp(path, ctx.Request.Method) )
-        else
-            Controller.redirect ctx path
+  let redirect ctx path : HttpFuncResult=
+    if isXhr ctx then
+      ctx.SetStatusCode 200
+      js(ctx, turbolinksResp(path, ctx.Request.Method) )
+    else
+      Controller.redirect ctx path
 
-    let internal handleTurbolinks (ctx: HttpContext) =
-        if isTurbolink ctx then
-            ctx.Response.Headers.Add("Turbolinks-Location", StringValues ctx.Request.Path.Value)
+  let internal handleTurbolinks (ctx: HttpContext) =
+    if isTurbolink ctx then
+      ctx.Response.Headers.Add("Turbolinks-Location", StringValues ctx.Request.Path.Value)
 
 ///HttpHandler enabling Turbolinks support for given pipelines
 let turbolinks (nxt : HttpFunc) (ctx : HttpContext) : HttpFuncResult =
-    TurbolinksHelpers.handleTurbolinks ctx
-    nxt ctx
+  TurbolinksHelpers.handleTurbolinks ctx
+  nxt ctx
 
 type TurbolinksMiddleware (next: RequestDelegate) =
-    member __.Invoke(ctx: HttpContext) =
-        ctx.Response.OnStarting((fun ctx ->
-            TurbolinksHelpers.handleTurbolinks (unbox<_> ctx)
-            Task.CompletedTask
-        ), ctx)
-        next.Invoke(ctx)
+  member __.Invoke(ctx: HttpContext) =
+    ctx.Response.OnStarting((fun ctx ->
+      TurbolinksHelpers.handleTurbolinks (unbox<_> ctx)
+      Task.CompletedTask
+    ), ctx)
+    next.Invoke(ctx)
 
 type ApplicationBuilder with
 
-    [<CustomOperation("use_turbolinks")>]
-    ///Enable turbolinks supports for whole application (all endpoints)
-    member __.UseTurbolinks(state) =
-        let middleware (app : IApplicationBuilder) =
-            app.UseMiddleware<TurbolinksMiddleware>()
+  [<CustomOperation("use_turbolinks")>]
+  ///Enable turbolinks supports for whole application (all endpoints)
+  member __.UseTurbolinks(state) =
+      let middleware (app : IApplicationBuilder) =
+        app.UseMiddleware<TurbolinksMiddleware>()
 
-        { state with
-            AppConfigs = middleware::state.AppConfigs
-        }
+      { state with AppConfigs = middleware::state.AppConfigs }
 
 
