35183e327d4f411265a602e600c4a596ae4de90d
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2018-12-10 14:44:27 +0100

    Add case_insensitive router tests

--- tests/Saturn.UnitTests/RouterTests.fs
+++ tests/Saturn.UnitTests/RouterTests.fs
@@ -56,3 +56,58 @@ let tests =
 
 
   ]
+
+let testCiRouter = router {
+  case_insensitive
+
+  get "/1" (text "HelloWorld")
+  get "/post/1" (text "Foo")
+  post "/post/1" (text "1")
+  post "/post/2" (text "2")
+}
+
+[<Tests>]
+let caseInsensitiveTest =
+  testList "Case Insensitve tests" [
+    testCase "POST to `/POST/1` returns `1`" <| fun _ ->
+      let ctx = getEmptyContext "POST" "/POST/1"
+
+      let expected = "1"
+      let result = testCiRouter next ctx |> runTask
+      match result with
+      | None -> failtestf "Result was expected to be %s" expected
+      | Some ctx ->
+        Expect.equal (getBody ctx) expected "Result should be equal"
+
+    testCase "POST to `/POST/2` returns `2`" <| fun _ ->
+      let ctx = getEmptyContext "POST" "/POST/2"
+
+      let expected = "2"
+      let result = testCiRouter next ctx |> runTask
+      match result with
+      | None -> failtestf "Result was expected to be %s" expected
+      | Some ctx ->
+        Expect.equal (getBody ctx) expected "Result should be equal"
+
+    testCase "GET to `/POST/1` returns `Foo`" <| fun _ ->
+      let ctx = getEmptyContext "GET" "/POST/1"
+
+      let expected = "Foo"
+      let result = testCiRouter next ctx |> runTask
+      match result with
+      | None -> failtestf "Result was expected to be %s" expected
+      | Some ctx ->
+        Expect.equal (getBody ctx) expected "Result should be equal"
+
+    testCase "GET to `1` returns `HelloWorld`" <| fun _ ->
+      let ctx = getEmptyContext "GET" "/1"
+
+      let expected = "HelloWorld"
+      let result = testCiRouter next ctx |> runTask
+      match result with
+      | None -> failtestf "Result was expected to be %s" expected
+      | Some ctx ->
+        Expect.equal (getBody ctx) expected "Result should be equal"
+
+
+  ]
