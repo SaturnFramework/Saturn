e4a48e1640688f4fb4b3c4bebd670dc6ea78c3b3
Chet Husk <chusk3@gmail.com>
2019-03-25 10:42:44 -0500

    minimize diff

--- src/Saturn/Utils.fs
+++ src/Saturn/Utils.fs
@@ -1,8 +1,6 @@
 namespace Saturn
 
 module Utils =
-  open System.Linq
-  open Microsoft.Extensions.DependencyInjection
 
   module String =
     let internal equalsCaseInsensitive (a : string) (b : string) =
