b25a7ff3de4f6f8d0c46fbfa159d1d6e2baafa72
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2019-07-25 00:06:19 +0200

    Add gRPC sample

---
+++ sample/gRpcSample.Client/Program.fs
@@ -0,0 +1,16 @@
+﻿open ProtoBuf.Grpc.Client
+open Shared
+open System
+open System.Net.Http
+open FSharp.Control.Tasks
+
+[<EntryPoint>]
+let main _ =
+    HttpClientExtensions.AllowUnencryptedHttp2 <- true
+    task {
+        use http = new HttpClient (BaseAddress = Uri "http://localhost:10042")
+        let client = http.CreateGrpcService<ICalculator>()
+        let! result = client.MultiplyAsync { X = 12; Y = 4 }
+        printfn "%i" result.Result
+        return 0
+    } |> fun t -> t.Result
