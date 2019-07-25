b621e028a2723773055db0ac80235df0f0d8b70d
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2019-07-25 17:29:03 +0200

    Add support for the constructor DI in gRPC

--- sample/gRpcSample/gRpcSample.fs
+++ sample/gRpcSample/gRpcSample.fs
@@ -6,15 +6,23 @@ open Microsoft.AspNetCore.Server.Kestrel.Core
 open Shared
 open System.Threading.Tasks
 
+open Giraffe.Serialization
+
 type MyCalculator() =
     interface ICalculator with
         member __.MultiplyAsync request =
             ValueTask<_> { Result = request.X * request.Y }
 
+type MyCalculatorWithDI(serializer: IJsonSerializer) =
+    interface ICalculator with
+        member __.MultiplyAsync request =
+            printfn "Multiply reques serialized: %s" (serializer.SerializeToString request)
+            ValueTask<_> { Result = request.X * request.Y }
+
 let app = application {
   no_router
   listen_local 10042 (fun opts -> opts.Protocols <- HttpProtocols.Http2)
-  use_grpc MyCalculator
+  use_grpc MyCalculatorWithDI
 }
 
 
