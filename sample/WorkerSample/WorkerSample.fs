050aeded898b49c155a867ea3e39b849e5b2fbb7
Jeremy Abbott <jeremymabbott@gmail.com>
2019-10-31 22:19:27 -0700

    Adds working sample

---
+++ sample/WorkerSample/WorkerSample.fs
@@ -0,0 +1,28 @@
+module Sample
+
+open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.Logging
+open FSharp.Control.Tasks.V2.ContextInsensitive
+open System.Threading
+open Microsoft.Extensions.DependencyInjection
+open Saturn
+
+type Worker(logger:ILogger<Worker>) =
+    inherit BackgroundService()
+    override __.ExecuteAsync(ct: CancellationToken) =
+            ct.Register(fun () -> logger.LogInformation("Worker canceled at: {time}", System.DateTimeOffset.Now)) |> ignore
+            task {
+                while not ct.IsCancellationRequested do
+                logger.LogInformation("Worker running at: {time}", System.DateTimeOffset.Now)
+                do! Tasks.Task.Delay(1000, ct)
+            } :> Tasks.Task
+
+[<EntryPoint>]
+let main argv =
+    let h =
+        host {
+            cli_arguments argv
+            service_config (fun s -> s.AddHostedService<Worker>())
+        }
+    run h
+    0 // return an integer exit code
