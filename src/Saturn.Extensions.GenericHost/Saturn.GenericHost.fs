49b6747c067d56a82e7fabefaf6a0099e0be95ca
Jeremy Abbott <jeremymabbott@gmail.com>
2019-10-31 22:19:17 -0700

    Builds out minimum operations for registering a worker

--- src/Saturn.Extensions.GenericHost/Saturn.GenericHost.fs
+++ src/Saturn.Extensions.GenericHost/Saturn.GenericHost.fs
@@ -4,14 +4,8 @@ open FSharp.Control.Tasks.V2.ContextInsensitive
 open System.Threading
 open Microsoft.Extensions.DependencyInjection
 
+[<AutoOpen>]
 module GenericHost =
-    type Worker() =
-        inherit BackgroundService()
-        override __.ExecuteAsync(ct: CancellationToken) =
-            task {
-                while not ct.IsCancellationRequested do
-                do! Tasks.Task.Delay(1000, ct)
-            } :> Tasks.Task
 
     type HostState = {
         ServiceConfigs: (IServiceCollection -> IServiceCollection) list
@@ -22,7 +16,30 @@ module GenericHost =
     type HostBuilderCE internal () =
         member __.Yield(_) =
             { HostConfigs = []; ServiceConfigs = []; CliArguments = None }
-        member __.Run(state: HostState) : IHostBuilder =
-            let defaultHost = Host.CreateDefaultBuilder()
 
-            defaultHost
+        member __.Run(state: HostState) : IHostBuilder =
+            let defaultHost =
+                Host.CreateDefaultBuilder(Option.toObj state.CliArguments)
+                |> List.foldBack (fun e acc -> e acc ) state.HostConfigs
+
+            defaultHost.ConfigureServices(fun _ services ->
+                 state.ServiceConfigs |> List.rev |> List.iter (fun fn -> fn services |> ignore) |> ignore)
+
+        ///Adds custom host configuration step.
+        [<CustomOperation("host_config")>]
+        member __.HostConfig(state, config) =
+          {state with HostConfigs = config::state.HostConfigs}
+
+        ///Adds custom service configuration step.
+        [<CustomOperation("service_config")>]
+        member __.ServiceConfig(state, config) =
+          {state with ServiceConfigs = config::state.ServiceConfigs}
+
+        ///Sets the cli arguments for the `IWebHostBuilder` to enable default command line configuration and functionality.
+        [<CustomOperation("cli_arguments")>]
+        member __.CliArguments (state, args) =
+           { state with CliArguments = Some args }
+
+    let host = HostBuilderCE()
+
+    let run (hostBuilder:IHostBuilder) = hostBuilder.Build().Run()
