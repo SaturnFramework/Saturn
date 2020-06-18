72a8c4bc689ffead1c3acf9fd200e37320681fb2
Krzysztof Cieslak <krzysztof_cieslak@windowslive.com>
2020-06-18 14:00:19 +0200

    Add performance benchmark using wrk

---
+++ benchmark/SaturnBench/Program.fs
@@ -0,0 +1,26 @@
+﻿open System
+open Giraffe.ResponseWriters
+open Giraffe.Core
+open Saturn
+open Microsoft.Extensions.Logging
+
+let topRouter = router {
+    get "/" (text "")
+    getf "/user/%s" text
+    post "/user" (text "")
+
+    not_found_handler (setStatusCode 404 >=> text "Not Found")
+}
+
+let app = application {
+  use_router topRouter
+  logging (fun loggerBuilder ->
+    loggerBuilder.ClearProviders()
+    |> ignore
+  )
+}
+
+[<EntryPoint>]
+let main _ =
+    run app
+    0
