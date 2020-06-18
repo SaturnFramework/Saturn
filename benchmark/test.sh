cb05f003005f3516a6174766a36c6beacfb7b436
Krzysztof Cieslak <krzysztof_cieslak@windowslive.com>
2020-06-18 14:00:19 +0200

    Add performance benchmark using wrk

---
+++ benchmark/test.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+pkill -f dotnet
+
+dotnet build ./GiraffeBench/GiraffeBench.fsproj -c Release
+wait $!
+echo 'Giraffe build'
+dotnet build ./SaturnBench/SaturnBench.fsproj -c Release
+wait $!
+echo 'Saturn build'
+
+sh -c 'dotnet GiraffeBench/bin/Release/netcoreapp3.1/GiraffeBench.dll' &
+G_PID=$!
+echo ''
+sleep 3
+echo ''
+echo 'Testing Giraffe'
+wrk -t12 -c400 -d30s http://127.0.0.1:5000/user/test
+echo ''
+echo ''
+
+kill -9 $G_PID
+echo 'Giraffe tested'
+
+sleep 5
+echo ''
+
+sh -c 'dotnet SaturnBench/bin/Release/netcoreapp3.1/SaturnBench.dll' &
+S_PID=$!
+echo ''
+sleep 3
+echo ''
+echo 'Testing Saturn'
+wrk -t12 -c400 -d30s http://127.0.0.1:5000/user/test
+echo ''
+echo ''
+kill -9 $S_PID
+echo 'Saturn tested'
+
+sleep 5
