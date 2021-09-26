7b89021fc79a1a7e9d9db56a9613680a50a97bf3
Joshua Passos <joshuapassos@gmail.com>
2021-09-25 23:39:06 -0300

    Change grpc samples

--- sample/gRpcSample.Client/Program.fs
+++ sample/gRpcSample.Client/Program.fs
@@ -3,14 +3,21 @@ open Shared
 open System
 open System.Net.Http
 open FSharp.Control.Tasks
+open Grpc.Net.Client
 
 [<EntryPoint>]
 let main _ =
     HttpClientExtensions.AllowUnencryptedHttp2 <- true
     task {
-        use http = new HttpClient (BaseAddress = Uri "http://localhost:10042")
-        let client = http.CreateGrpcService<ICalculator>()
-        let! result = client.MultiplyAsync { X = 12; Y = 4 }
+        use http = GrpcChannel.ForAddress("http://localhost:10042");
+        let calculatorClient = http.CreateGrpcService<ICalculator>()
+        let helloClient = http.CreateGrpcService<IHello>()
+
+        let! result = calculatorClient.MultiplyAsync { X = 12; Y = 4 }
         printfn "%i" result.Result
+        
+        let! r = helloClient.TestAsync { Parameter = "Saturn" }
+        printfn "%s" r.Response
+        
         return 0
     } |> fun t -> t.Result
--- sample/gRpcSample/gRpcSample.fs
+++ sample/gRpcSample/gRpcSample.fs
@@ -9,24 +9,32 @@ open System.Threading.Tasks
 open Giraffe
 
 type MyCalculator() =
-    interface ICalculator with
-        member __.MultiplyAsync request =
-            ValueTask<_> { Result = request.X * request.Y }
+  interface ICalculator with
+    member __.MultiplyAsync request =
+      ValueTask<_> { Result = request.X * request.Y }
 
 type MyCalculatorWithDI(serializer: Json.ISerializer) =
-    interface ICalculator with
-        member __.MultiplyAsync request =
-            printfn "Multiply reques serialized: %s" (serializer.SerializeToString request)
-            ValueTask<_> { Result = request.X * request.Y }
+  interface ICalculator with
+    member __.MultiplyAsync request =
+      printfn "Multiply requests serialized: %s" (serializer.SerializeToString request)
+      ValueTask<_> { Result = request.X * request.Y }
 
-let app = application {
-  no_router
-  listen_local 10042 (fun opts -> opts.Protocols <- HttpProtocols.Http2)
-  use_grpc MyCalculatorWithDI
-}
+
+type MyHelloWithDI(serializer: Json.ISerializer) =
+  interface IHello with
+    member __.TestAsync request =
+      ValueTask<_> { Response = $"Hello, {request.Parameter}!" }
+
+let app =
+  application {
+    no_router
+    listen_local 10042 (fun opts -> opts.Protocols <- HttpProtocols.Http2)
+    use_grpc MyHelloWithDI
+    use_grpc MyCalculatorWithDI
+  }
 
 
 [<EntryPoint>]
 let main _ =
-    run app
-    0 // return an integer exit code
+  run app
+  0 // return an integer exit code
